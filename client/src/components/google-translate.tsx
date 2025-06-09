b25316a1ef870ebe28b8ae1767cf191d088b546a
ʙʀᴀɴᴅᴏɴ ᴛᴏʀʀᴇɢʟᴏꜱᴀ <brandontorreglosa@gmail.com>
2025-06-10 02:07:39 +0300

    Update google-translate.tsx

--- client/src/components/google-translate.tsx
+++ client/src/components/google-translate.tsx
@@ -21,15 +21,11 @@ export default function GoogleTranslate() {
       .catch(() => setUserCountry(null));
   }, []);
 
-  // Compressed language list into a single string (Google Translate language codes)
   const includedLanguages = [
-    "af","sq","am","ar","hy","az","eu","be","bn","bs","bg","ca","ceb","zh-CN","zh-TW","co","hr",
-    "cs","da","nl","en","eo","et","fi","fr","fy","gl","ka","de","el","gu","ht","ha","haw","he",
-    "hi","hmn","hu","is","ig","id","ga","it","ja","jv","kn","kk","km","rw","ko","ku","ky","lo",
-    "la","lv","lt","lb","mk","mg","ms","ml","mt","mi","mr","mn","my","ne","no","ny","or","ps",
-    "fa","pl","pt","pa","ro","ru","sm","gd","sr","st","sn","sd","si","sk","sl","so","es","su",
-    "sw","sv","tl","tg","ta","tt","te","th","tr","tk","uk","ur","ug","uz","vi","cy","xh","yi",
-    "yo","zu"
+    "fr","de","nl","it","es","pt","pt-PT","pt-BR","ca","gl","oc",
+    "sv","no","nb","nn","da","fi","is","et","lv","lt",
+    "pl","cs","sk","hu","sl","hr","sr","bs","mk","bg","ro","sq",
+    "el","tr","ru","uk","be","az","hy","ka","mt","fa","kk","ky","eu","fo"
   ].join(",");
 
   const initializeGoogleTranslate = () => {
